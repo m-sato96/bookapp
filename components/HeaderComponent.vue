59f74932dfcfb448f2eb9cd01530c2f26b3b6268
m-sato <ssmmaagg36@gmail.com>
2023-02-01 10:43:55 +0900

    Header,Footerのコンポーネント作成

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-app-bar app elevate-on-scroll elevation="4">
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'BookApp',
+    }
+  },
+}
+</script>
+
+<style></style>
